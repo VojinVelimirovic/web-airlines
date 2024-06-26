413a6e6181979382f1cc5d36f6506856ee73f047
VojinVelimirovic <vojinvelimirovic2002@gmail.com>
2024-06-26 22:41:46 +0200

    v0.17

--- Sistem za rezervaciju avio karata/Sistem za rezervaciju avio karata/Controllers/ReviewsController.cs
+++ Sistem za rezervaciju avio karata/Sistem za rezervaciju avio karata/Controllers/ReviewsController.cs
@@ -10,6 +10,10 @@ namespace Sistem_za_rezervaciju_avio_karata.Controllers
 {
     public class ReviewsController : ApiController
     {
+        public List<Review> Get()
+        {
+            return Reviews.ReviewsList;
+        }
         [HttpPost]
         [Route("api/reviews/add")]
         public IHttpActionResult AddReview(Review review)
@@ -17,6 +21,31 @@ namespace Sistem_za_rezervaciju_avio_karata.Controllers
             review.User.Reservations.Clear();
             Reviews.AddReview(review);
 
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("api/reviews/update")]
+        public IHttpActionResult UpdateReview(Review review)
+        {
+            var airline = Airlines.AirlinesList.FirstOrDefault(f => f.Id == review.Airline.Id);
+            if (airline != null)
+            {
+                var reviewFromAirline = airline.Reviews.FirstOrDefault(f => f.Id == review.Id);
+                if (reviewFromAirline != null)
+                {
+                    reviewFromAirline.Status = review.Status;
+                }
+            }
+
+            var reviewToUpdate = Reviews.ReviewsList.FirstOrDefault(f => f.Id == review.Id);
+            if (reviewToUpdate != null)
+            {
+                reviewToUpdate.Status = review.Status;
+            }
+
+            Airlines.SaveAirlines();
+            Reviews.SaveReviews();
             return Ok();
         }
     }
